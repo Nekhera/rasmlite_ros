05ad05f433a2a46c623ad8c00b4acc8f711e3e2d
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2020-05-28 14:11:42 -0400

    Initial Commit

---
+++ src/decimation_node.cpp
@@ -0,0 +1,15 @@
+#include "RASMlite/mapper/decimation.h"
+
+using namespace pcl;
+
+ros::Publisher point_pub;
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "decimation_node");
+  ros::NodeHandle nh;
+  point_pub = nh.advertise<PointCloud<PointXYZRGB>>("/points/decimated", 1);
+
+  nh.subscribe("/points", 1, RASMlite::decimation::pointcloudCallback);
+  ros::spin();
+  return 0;
+}
