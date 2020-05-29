08fd25864a5cf1d5afd1b70adde07e5dc782b00c
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2020-05-28 20:47:23 -0400

    ROS pipeline functional

--- src/decimation_node.cpp
+++ src/decimation_node.cpp
@@ -1,15 +1,22 @@
+#include <ros/ros.h>
 #include "RASMlite/mapper/decimation.h"
 
 using namespace pcl;
 
 ros::Publisher point_pub;
 
+void pointcloudCallback(const PointCloud<PointXYZ>::ConstPtr &cloud) {
+  PointCloud<PointXYZ>::Ptr cloud_ref(new PointCloud<PointXYZ>(*cloud));
+  RASMlite::decimation::process(cloud_ref);
+  point_pub.publish(cloud_ref);
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "decimation_node");
   ros::NodeHandle nh;
-  point_pub = nh.advertise<PointCloud<PointXYZRGB>>("/points/decimated", 1);
+  ros::Subscriber sub_cloud = nh.subscribe<PointCloud<PointXYZ>>("/points2", 1, pointcloudCallback);
 
-  nh.subscribe("/points2", 1, RASMlite::decimation::pointcloudCallback);
+  point_pub = nh.advertise<PointCloud<PointXYZRGB>>("/points/decimated", 1);
   ros::spin();
   return 0;
 }
